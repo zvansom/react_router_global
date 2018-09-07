1b9932bebecefca7991402a02ba51f8b71419ef3
Zach VanSomeren <zachvansomeren@Zachs-MBP.local.tld>
2018-09-07 07:12:43 -0700

    Added fetch to get random food title and image.

--- react-app/src/Food.js
+++ react-app/src/Food.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Food = () => (
-  <div>
-    <h1>FOOD PAGE</h1>
-    <p>Nihil saepe molestiae cupiditate facilis non dignissimos velit. Exercitationem et ut id eos architecto dolores unde omnis. Ex non voluptatem nam pariatur harum quis esse.</p>
-  </div>
-);
+export default class Food extends Component {
+  state = {
+    food: {},
+  }
 
-export default Food;
+  async componentDidMount() {
+    const searchTerm = 'chicken';
+    const API_KEY = '6263211e212479e5d6d19ed2ea07a56b';
+    const endpoint = 'https://www.food2fork.com/api/search?key=';
+    try {
+      const res = await fetch(`${endpoint}${API_KEY}&q=${searchTerm}`);
+      const recipes = await res.json();
+
+      this.setState({
+        food: recipes.recipes[Math.floor(Math.random() * (20))],
+      });
+    } catch (e) { console.log(e); }
+  }
+
+  render() {
+    const { food } = this.state;
+    return (
+      <div>
+        <h1>FOOD PAGE</h1>
+        <p>Here's a food!</p>
+
+        <h2>{food.title}</h2>
+        <img src={food.image_url} alt={food.title} />
+      </div>
+    );
+  }
+}
